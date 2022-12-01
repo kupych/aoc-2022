bde2d6299ba95a46531d21619938beaea372a673
Stefan Kupych <stefankupych@hifyre.com>
2022-12-01 00:09:23 -0500

    Day 1

--- lib/day1.ex
+++ lib/day1.ex
@@ -10,16 +10,20 @@ defmodule Aoc.Day1 do
   def day(), do: 1
 
   @impl Day
-  def a(depths) do
-    number_of_increases(depths)
+  def a(calories) do
+    calories
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort(&(&1 >= &2))
+    |> hd()
   end
 
   @impl Day
-  def b(depths) do
-    depths
-    |> Enum.chunk_every(3, 1, :discard)
+  def b(calories) do
+    calories
     |> Enum.map(&Enum.sum/1)
-    |> number_of_increases()
+    |> Enum.sort(&(&1 >= &2))
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 
   @impl Day
@@ -27,19 +31,14 @@ defmodule Aoc.Day1 do
     with {:ok, file} <- Day.load(__MODULE__) do
       file
       |> String.trim()
-      |> String.split("\n")
-      |> Enum.map(&String.to_integer/1)
+      |> String.split("\n\n", trim: true)
+      |> Enum.map(&parse_calorie_count/1)
     end
   end
 
-  @doc """
-  `number_of_increases/1` takes a list of integers and
-  finds the number of times a value increases from its
-  previous value.
-  """
-  def number_of_increases([_ | _] = values) do
-    values
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.count(fn [a, b] -> b > a end)
+  defp parse_calorie_count(calories) when is_binary(calories) do
+    calories
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 end
