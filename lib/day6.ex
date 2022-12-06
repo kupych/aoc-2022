b418a1e1b1f572c7e49570a06c2b80bf4c13358a
Stefan Kupych <stefankupych@hifyre.com>
2022-12-06 00:13:42 -0500

    Day 6

--- lib/day6.ex
+++ lib/day6.ex
@@ -10,19 +10,38 @@ defmodule Aoc.Day6 do
   def day(), do: 6
 
   @impl Day
-  def a(_) do
-    :ok
+  def a(signal) do
+    parse(signal, 4)
   end
 
   @impl Day
-  def b(_) do
-    :ok
+  def b(signal) do
+    parse(signal, 14)
   end
 
   @impl Day
   def parse_input() do
     with {:ok, file} <- Day.load(__MODULE__) do
-      file
+      String.split(file, "", trim: true)
     end
+  end
+
+  def parse(list, size) do
+    parse(list, size, size)
+  end
+
+  def parse([_ | rest] = list, size, count) do
+    list
+    |> Enum.take(size)
+    |> Enum.uniq()
+    |> Enum.count()
+    |> case do
+      ^size -> count
+      _ -> parse(rest, size, count + 1)
+    end
+  end
+
+  def parse(_, _, count) do
+    count
   end
 end
