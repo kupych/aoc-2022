b8761d582426eaec6fdb11b083497c557c5033fb
Stefan Kupych <stefan@hifyre.com>
2023-12-02 22:54:41 -0500

    Day 16 partial

--- lib/day16.ex
+++ lib/day16.ex
@@ -10,8 +10,9 @@ defmodule Aoc.Day16 do
   def day(), do: 16
 
   @impl Day
-  def a(_) do
-    ""
+  def a(valves) do
+    IO.inspect valves
+    :ok
   end
 
   @impl Day
@@ -22,7 +23,41 @@ defmodule Aoc.Day16 do
   @impl Day
   def parse_input() do
     with {:ok, file} <- Day.load(__MODULE__) do
-      file
+      graph = :digraph.new()
+
+        file
+        |> String.split("\n", trim: true)
+        |> Enum.reduce({%{}, graph}, &parse_valve/2)
+    end
+  end
+
+  def parse_valve(string, {map, graph}) do
+    [_, valve, rate, tunnels] = Regex.run(~r/Valve ([A-Z]{2}).*?(\d+).*?valves? (.*)/, string)
+
+    :digraph.add_vertex(graph, valve)
+    tunnels = String.split(tunnels, ", ")
+
+    Enum.each(tunnels, &:digraph.add_vertex(graph, &1))
+    tunnels
+      |> Enum.map(&[graph, valve, &1])
+      |> Enum.each(&apply(:digraph, :add_edge, &1))
+
+    {Map.put(map, valve, String.to_integer(rate)), graph}
+  end
+
+  def open_valves(valves, visited \\ ["AA"])
+
+  def open_valves(_, path) when length(path) == 30 do
+    path
+  end
+
+  def open_valves(valves, [current | visited] = path) do
+    valves
+    |> get_in([current, :tunnels])
+    |> Kernel.--(visited)
+    |> case do
+      [] -> path
+      tunnels -> Enum.map(tunnels, &open_valves(valves, [&1 | path]))
     end
   end
 end
